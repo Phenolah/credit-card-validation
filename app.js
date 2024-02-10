ab7809f7edc0fd07198ea438952f1a9b5d881f69
Phenolah <104977409+Phenolah@users.noreply.github.com>
2024-02-10 08:28:03 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+function validateCreditCard(cardNumber) {
+    
+    const visaPattern = /^4[0-9]{12}(?:[0-9]{3})?$/; 
+    const mastercardPattern = /^5[1-5][0-9]{14}$/;
+    
+
+    
+    if (visaPattern.test(cardNumber)) {
+        return "Visa";
+    } else if (mastercardPattern.test(cardNumber)) {
+        return "Mastercard";
+    } 
+     else {
+        return "Invalid";
+    }
+}
+
+const cardNumber = "4111111111111111"; 
+console.log(validateCreditCard(cardNumber)); 
